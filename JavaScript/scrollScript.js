343842aac624b148e7e758c26a3f26e07930a267
ankit <ankit.gupta@kreeti.com>
2023-12-17 12:06:53 +0530

    updated app

---
+++ JavaScript/scrollScript.js
@@ -0,0 +1,13 @@
+gsap.to("#logo", {
+    scrollTrigger: {
+        trigger: "main",
+        // start: 1,
+        // scrub: "true"
+        scrub: 1
+    },
+    fontSize: 6,
+    y: -560,
+    x: -1000,
+    color: "rgb(36, 30, 26)",
+    duration: 1
+});
